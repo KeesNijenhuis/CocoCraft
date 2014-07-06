9ebba644f4a1aff86877aec350325b0759782ef4
Kees Nijenhuis <nijenhuis.kees@gmail.com>
2014-07-07 00:32:16 +0200

    Added Grinded ores.

--- src/main/java/nl/nijenhuis/cococraft/items/CocoCraftItems.java
+++ src/main/java/nl/nijenhuis/cococraft/items/CocoCraftItems.java
@@ -1,14 +1,37 @@
 package nl.nijenhuis.cococraft.items;
 
+import net.minecraft.item.Item;
+import nl.nijenhuis.cococraft.reference.Reference;
+import nl.nijenhuis.cococraft.utility.RegisterHelper;
+
 public class CocoCraftItems {
 
+    public static Item grindedCoco, grindedMithril, grindedSilver, grindedAdamant, grindedRunite, grindedIron, grindedGold;
+
     public static void init() {
+
+        grindedCoco = new ModItem().setUnlocalizedName("grindedCoco").setTextureName(Reference.RESOURCE + "grindedCoco");
+        grindedMithril = new ModItem().setUnlocalizedName("grindedMithril").setTextureName(Reference.RESOURCE + "grindedMithril");
+        grindedSilver = new ModItem().setUnlocalizedName("grindedSilver").setTextureName(Reference.RESOURCE + "grindedSilver");
+        grindedAdamant = new ModItem().setUnlocalizedName("grindedAdamant").setTextureName(Reference.RESOURCE + "grindedAdamant");
+        grindedRunite = new ModItem().setUnlocalizedName("grindedRunite").setTextureName(Reference.RESOURCE + "grindedRunite");
+        grindedIron = new ModItem().setUnlocalizedName("grindedIron").setTextureName(Reference.RESOURCE + "grindedIron");
+        grindedGold = new ModItem().setUnlocalizedName("grindedGold").setTextureName(Reference.RESOURCE + "grindedGold");
 
 
         registerItems();
     }
 
     public static void registerItems() {
+
+        RegisterHelper.registerItem(grindedCoco);
+        RegisterHelper.registerItem(grindedMithril);
+        RegisterHelper.registerItem(grindedSilver);
+        RegisterHelper.registerItem(grindedAdamant);
+        RegisterHelper.registerItem(grindedRunite);
+        RegisterHelper.registerItem(grindedIron);
+        RegisterHelper.registerItem(grindedGold);
+
 
     }
 }
---
+++ src/main/java/nl/nijenhuis/cococraft/items/ModItem.java
@@ -0,0 +1,12 @@
+package nl.nijenhuis.cococraft.items;
+
+import net.minecraft.item.Item;
+import nl.nijenhuis.cococraft.CocoCraft;
+
+public class ModItem extends Item {
+
+    public ModItem() {
+        super();
+        setCreativeTab(CocoCraft.tabCocoCraft);
+    }
+}

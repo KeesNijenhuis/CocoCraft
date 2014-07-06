3fd63b3cc86e3c41a99f8c30ca8a0ba7147a9f8f
KeesNijenhuis <nijenhuis.kees@gmail.com>
2014-07-06 21:46:27 +0200

    Initial Commit

---
+++ src/main/java/nl/nijenhuis/cococraft/CocoCraft.java
@@ -0,0 +1,28 @@
+package nl.nijenhuis.cococraft;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "CocoCraft", name = "CocoCraft", version = "1.7.10-1.0")
+public class CocoCraft {
+
+    @Mod.Instance("CocoCraft")
+    public static CocoCraft instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
+}

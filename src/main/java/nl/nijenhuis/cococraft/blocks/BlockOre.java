b602e96e312ef2bd497689da6524d53add4e08ff
Kees Nijenhuis <nijenhuis.kees@gmail.com>
2014-07-07 00:06:56 +0200

    Added the First ore, Coco Ore

---
+++ src/main/java/nl/nijenhuis/cococraft/blocks/BlockOre.java
@@ -0,0 +1,13 @@
+package nl.nijenhuis.cococraft.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import nl.nijenhuis.cococraft.CocoCraft;
+
+public class BlockOre extends Block {
+
+    public BlockOre() {
+        super(Material.iron);
+        setCreativeTab(CocoCraft.tabCocoCraft);
+    }
+}
--- src/main/java/nl/nijenhuis/cococraft/blocks/CocoCraftBlocks.java
+++ src/main/java/nl/nijenhuis/cococraft/blocks/CocoCraftBlocks.java
@@ -1,14 +1,22 @@
 package nl.nijenhuis.cococraft.blocks;
 
+import net.minecraft.block.Block;
+import nl.nijenhuis.cococraft.reference.Reference;
+import nl.nijenhuis.cococraft.utility.RegisterHelper;
+
 public class CocoCraftBlocks {
+
+    public static Block oreCoco, oreMithril, oreSilver, oreAdamant, oreRunite;
 
     public static void init() {
 
+        oreCoco = new BlockOre().setBlockName("oreCoco").setBlockTextureName(Reference.RESOURCE + "oreCoco");
 
         registerBlocks();
     }
 
     public static void registerBlocks() {
 
+        RegisterHelper.registerBlock(oreCoco);
     }
 }

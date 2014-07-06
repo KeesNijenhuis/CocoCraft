01d782d17fb22fd1e1ff94861ed9caee7508545a
Kees Nijenhuis <nijenhuis.kees@gmail.com>
2014-07-07 00:07:12 +0200

    Updated the Reference file

--- src/main/java/nl/nijenhuis/cococraft/reference/Reference.java
+++ src/main/java/nl/nijenhuis/cococraft/reference/Reference.java
@@ -9,5 +9,7 @@ public class Reference {
     public static final String CLIENT_PROXY_CLASS = "nl.nijenhuis.cococraft.proxy.ClientProxy";
     public static final String GUI_FACTORY_CLASS = "nl.nijenhuis.cococraft.client.gui.GuiFactory";
 
+    public static final String RESOURCE = MOD_ID + ":";
+
 
 }

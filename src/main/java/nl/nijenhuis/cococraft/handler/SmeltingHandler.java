22bedffab9eeb97059814879469a762a1e30c719
Kees Nijenhuis <nijenhuis.kees@gmail.com>
2014-07-07 21:45:42 +0200

    Added all the recipes

--- src/main/java/nl/nijenhuis/cococraft/handler/SmeltingHandler.java
+++ src/main/java/nl/nijenhuis/cococraft/handler/SmeltingHandler.java
@@ -11,8 +11,18 @@ public class SmeltingHandler {
 
         if(ConfigurationHandler.hardRecipes == true) {
             GameRegistry.addSmelting(CocoCraftItems.grindedCoco, new ItemStack(CocoCraftItems.nuggetCoco, 1), 0.1F);
+            GameRegistry.addSmelting(CocoCraftItems.grindedMithril, new ItemStack(CocoCraftItems.nuggetMithril, 1), 0.1F);
+            GameRegistry.addSmelting(CocoCraftItems.grindedSilver, new ItemStack(CocoCraftItems.nuggetSilver, 1), 0.1F);
+            GameRegistry.addSmelting(CocoCraftItems.grindedAdamant, new ItemStack(CocoCraftItems.nuggetAdamant, 1), 0.1F);
+            GameRegistry.addSmelting(CocoCraftItems.grindedRunite, new ItemStack(CocoCraftItems.nuggetRunite, 1), 0.1F);
+
         } else {
             GameRegistry.addSmelting(CocoCraftItems.grindedCoco, new ItemStack(CocoCraftItems.ingotCoco, 1), 0.1F);
+            GameRegistry.addSmelting(CocoCraftItems.grindedMithril, new ItemStack(CocoCraftItems.ingotMithril, 1), 0.1F);
+            GameRegistry.addSmelting(CocoCraftItems.grindedSilver, new ItemStack(CocoCraftItems.ingotSilver, 1), 0.1F);
+            GameRegistry.addSmelting(CocoCraftItems.grindedAdamant, new ItemStack(CocoCraftItems.ingotAdamant, 1), 0.1F);
+            GameRegistry.addSmelting(CocoCraftItems.grindedRunite, new ItemStack(CocoCraftItems.ingotRunite, 1), 0.1F);
+
         }
     }
 }
